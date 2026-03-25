719ee29da3bf76c9168b7a14ed1ab6fc76a9a507
Datdo010905 <dotiendat092005@gmail.com>
2026-03-25 11:38:18 +0700

    thêm dichvuSchema

---
+++ QuanLyLichHen_FE/src/utils/dichVuSchema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const dichVuSchema = z.object({
+  serviceID: z.string().min(1, { message: "Mã dịch vụ không được để trống" }),
+  serviceName: z.string().min(1, { message: "Tên dịch vụ không được để trống" }),
+  serviceType: z.string(),
+  serviceDesc: z.string().optional(), // optional: không bắt buộc nhập
+  serviceTime: z.coerce.number().min(5, { message: "Thời gian ít nhất 5 phút" }),
+  servicePrice: z.coerce.number().min(1000, { message: "Giá tiền phải từ 1.000đ" }), 
+  serviceStatus: z.string(),
+  serviceProcedure: z.string().optional(),
+});
+
+export type DichVuFormValues = z.infer<typeof dichVuSchema>;
