caddeb953bd8d9841aa3baf7257bdf2d54f90975
Datdo010905 <dotiendat092005@gmail.com>
2026-04-08 15:15:31 +0700

    đẩy data của api lên trang dashboard để xem tổng

--- QuanLyLichHen_FE/src/components/ui/StatCard.tsx
+++ QuanLyLichHen_FE/src/components/ui/StatCard.tsx
@@ -3,20 +3,22 @@ import React  from "react";
 // Khai báo các thuộc tính StatCard 
 interface StatCardProps {
     title: string;               // Tiêu đề (VD: "Tổng khách")
+    icon: string;
     value: string | number;      // Giá trị (VD: 150 hoặc "99.000đ")
     valueColor?: string;         // (Tùy chọn) Màu sắc của số, dùng cho chữ tiền màu xanh
     subText?: React.ReactNode;   // (Tùy chọn) Chữ nhỏ ở dưới cùng (VD: "Thành công: 310")
 }
   
-const StatCard: React.FC<StatCardProps> = ({ title, value, valueColor, subText }) => {
+const StatCard: React.FC<StatCardProps> = ({ title, icon, value, valueColor, subText }) => {
     return (
         <div className="card">
-            <h3>{title}</h3>
+            <h3><i className={icon}></i> {title}</h3>
             {/* Nếu có thì truyền valueColor, không thì mặc định CSS */}
             <p style={{ color: valueColor }}>{value}</p>
             
             {/* Nếu có thì truyền subText thì mới hiển thị thẻ <small> */}
             {subText && <small>{subText}</small>}
+            
         </div>
     );
 };
--- QuanLyLichHen_FE/src/pages/admin/DashBoardPage.tsx
+++ QuanLyLichHen_FE/src/pages/admin/DashBoardPage.tsx
@@ -4,21 +4,50 @@ import React, { useEffect, useState } from "react";
 import Modal from "../../components/ui/Modal";
 import CustomerApi from "../../api/customerApi";
 import BookingApi, { Booking } from "../../api/bookingApi";
+import StaffApi from "../../api/staffApi"
+import dichVuApi from "../../api/dichvuApi";
+import TaiKhoanApi from "../../api/taikhoanApi";
+import KhuyenMaiApi from "../../api/khuyenmaiApi";
 const DashboardPage: React.FC = () => {
 
     //lưu state để dùng
     const [totalCustomers, setTotalCustomers] = useState(0);
+    const [totalNV, setTotalNV] = useState(0);
+    const [totalDV, setTotalDV] = useState(0);
+    const [totalTK, setTotalTK] = useState(0);
+    const [totalKM, setTotalKM] = useState(0);
     const [bookingsToday, setTotalBookingsToday] = useState(0);
+    const [time, setTime] = useState("");
 
     const today = new Date().toISOString().split('T')[0];
+    const hour = new Date().getHours();
 
-
+    const [xinchao, setxinchao] = useState<string>("");
 
     const fetchData = async () => {
         const resKH = await CustomerApi.getAll();
+        const resNV = await StaffApi.getAll();
+        const resDV = await dichVuApi.getAll();
+        const resCSD = await dichVuApi.getAllCSD();
+        const resTK = await TaiKhoanApi.getAll();
+        const resKM = await KhuyenMaiApi.getAll();
+
 
         if (resKH.data.success) {
             setTotalCustomers(resKH.data.data.length);
+        }
+        if (resKM.data.success) {
+            setTotalKM(resKM.data.data.length);
+        }
+        if (resTK.data.success) {
+            setTotalTK(resTK.data.data.length);
+        }
+
+        if (resNV.data.success) {
+            setTotalNV(resNV.data.data.length);
+        }
+        if (resDV.data.success && resCSD.data.success) {
+            setTotalDV(resDV.data.data.length + resCSD.data.data.length);
         }
         const resBooking = await BookingApi.getAll();
         if (resBooking.data.success) {
@@ -28,44 +57,70 @@ const DashboardPage: React.FC = () => {
             );
             setTotalBookingsToday(Todaybookings.length);
         }
+
+        if (hour < 12) {
+            setxinchao("Good morning");
+        }
+        else if (hour < 18) {
+            setxinchao("Good afternoon");
+        }
+        else {
+            setxinchao("Good evening");
+        }
+
+
     };
     // Tải dữ liệu khi component mount
     useEffect(() => {
         fetchData();
+        const interval = setInterval(() => {
+            const now = new Date();
+            setTime(now.toLocaleTimeString("vi-VN"));
+        }, 1000);
+
+        return () => clearInterval(interval);
     }, []);
     return (
         <>
             <div id="dashboard" className="section active-section">
+                <div className="panel">
+                    <h2>Hello! {xinchao}, bây giờ là {time}</h2>
+                </div>
                 <div className="cards">
                     <StatCard
-                        title="Tổng khách"
-                        value={totalCustomers}
-                    />
-                    <StatCard
                         title="Đặt lịch hôm nay"
+                        icon="fas fa-calendar-check"
                         value={bookingsToday}
                         subText={`Cập nhật ngày: ${today}`}
                     />
+                    <StatCard
+                        title="Tổng khách"
+                        icon="fas fa-users"
+                        value={totalCustomers}
+                    />
+                    <StatCard
+                        title="Tổng nhân viên"
+                        icon="fas fa-user-tie"
+                        value={totalNV}
+                    />
+                    <StatCard
+                        title="Tổng dịch vụ"
+                        icon="fas fa-concierge-bell"
+                        value={totalDV}
+                    />
+                    <StatCard
+                        title="Tổng khuyến mại"
+                        icon="fas fa-tags"
+                        value={totalKM}
+                    />
+                    <StatCard
+                        title="Tổng người dùng"
+                        icon="fas fa-user-shield"
+                        value={totalTK}
+                    />
                 </div>
-                <div className="panel">
-                    <h2>Hoạt động gần đây</h2>
-                    <table className="table" id="recentTable">
-                        <thead>
-                            <tr>
-                                <th>Thời gian</th>
-                                <th>Sự kiện</th>
-                                <th>Người dùng</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Thời gian</td>
-                                <td>Sự kiện</td>
-                                <td>Người dùng</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+
+
             </div>
         </>
     );
